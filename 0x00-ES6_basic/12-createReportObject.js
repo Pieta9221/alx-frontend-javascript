38ed53896af43ef3773a30551bcc216bebe907b8
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2023-03-21 00:27:51 +0100

    Create 12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,15 @@
+export default function createReportObject(employeesList) {
+
+  return {
+
+    allEmployees: {
+
+      ...employeesList,
+
+    },
+
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+
+  };
+
+}
